28f0f3b8f2c32aceacf4972304f4e8c54f7c3c14
David Hohn <hohn.david@gmail.com>
2026-03-13 14:24:24 +0100

    try relative start_url

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
         theme_color: '#16a34a',
         background_color: '#ffffff',
         display: 'standalone',
-        start_url: process.env.VITE_START_PATH || '/',
+        start_url: process.env.VITE_START_PATH || '.',
         icons: [
           {
             src: 'icons/icon-192.png',
